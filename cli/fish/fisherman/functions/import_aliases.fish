f89ba7bd18331a32eda43d89dfd32b2f6a674597
William Albright <ping@ewilli.am>
2016-02-11 16:56:23 -0800

    check for argument in alias import

--- cli/fish/fisherman/functions/import_aliases.fish
+++ cli/fish/fisherman/functions/import_aliases.fish
@@ -1,15 +1,19 @@
 # fish functions much faster than alias eval
 function import_aliases --description 'bash aliases to .fish function files.'
-  for a in (cat ~/.dotfiles/cli/fish/$argv  | grep "^alias")
-    set aname (echo $a | grep -Eoe "[a-z0-9]+=" | sed 's/=//')
-    set command (echo $a | sed 's/^alias .*=//' \
-      | sed 's/^ *\'//' | sed 's/\' *$//' )
-    echo "Processing alias $aname as $command"
-    if test -f ~/.config/fish/functions/$aname.fish
-      echo Function $aname is already defined. Skipping...
-    else
-      alias $aname $command
-      funcsave $aname
+  if test -n "$argv"
+    for a in (cat $argv | grep "^alias")
+      set aname (echo $a | grep -Eoe "[a-z0-9]+=" | sed 's/=//')
+      set command (echo $a | sed 's/^alias .*=//' \
+        | sed 's/^ *\'//' | sed 's/\' *$//' )
+      echo "Processing alias $aname as $command"
+      if test -f ~/.config/fish/functions/$aname.fish
+        echo Function $aname is already defined. Skipping...
+      else
+        alias $aname $command
+        funcsave $aname
+      end
     end
+  else
+    echo (tint: red (bold: 'Pick a file.'))
   end
 end
