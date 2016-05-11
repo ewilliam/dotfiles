b861ecc1ede27239d374219d2f387f26bec19a03
William Albright <ping@ewilli.am>
2016-05-11 16:47:57 -0700

    add python environment management and update default ruby version

--- cli/fish/config.fish
+++ cli/fish/config.fish
@@ -1,13 +1,19 @@
 set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
+
 set -x EDITOR nvim
 
-fish_vi_mode
+# Python version/envirnoment management
+# https://github.com/adambrenecki/virtualfish
+# set -x PIP_REQUIRE_VIRTUALENV true
+set -x WORKON_HOME ~/.virtualenvs
+eval (python -m virtualfish compat_aliases auto_activation global_requirements)
 
+# Ruby version management
 # https://github.com/JeanMertz/chruby-fish
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
-chruby 2.3
+chruby 2.3.1
 
 # https://github.com/nvbn/thefuck/wiki/shell-aliases
 eval (thefuck --alias | tr '\n' ';')
@@ -15,4 +21,7 @@ eval (thefuck --alias | tr '\n' ';')
 # teamocil autocompletion
 complete -c teamocil -a "(teamocil --list)"
 
+fish_vi_mode
+
 source $fisher_home/config.fish
+source ~/.secrets.fish
