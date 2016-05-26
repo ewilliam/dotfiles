810c3b6b93611258f93b8d30d635508aaafac7e8
William Albright <ping@ewilli.am>
2016-05-25 22:41:13 -0700

    restructure fish and fisherman folders

--- cli/fish/config.fish
+++ cli/fish/config.fish
@@ -1,10 +1,6 @@
 set -x EDITOR nvim
 
-# Python version/envirnoment management
-# https://github.com/adambrenecki/virtualfish
-# set -x PIP_REQUIRE_VIRTUALENV true
-set -x WORKON_HOME ~/.virtualenvs
-eval (python -m virtualfish compat_aliases auto_activation global_requirements)
+fish_vi_key_bindings
 
 # Ruby version management
 # https://github.com/JeanMertz/chruby-fish
@@ -12,12 +8,19 @@ source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
 chruby 2.3.1
 
-# https://github.com/nvbn/thefuck/wiki/shell-aliases
-eval (thefuck --alias | tr '\n' ';')
+# Python version/envirnoment management
+# https://github.com/adambrenecki/virtualfish
+# set -x PIP_REQUIRE_VIRTUALENV true
+set -x WORKON_HOME ~/.virtualenvs
+eval (python -m virtualfish compat_aliases auto_activation global_requirements)
 
 # teamocil autocompletion
 complete -c teamocil -a "(teamocil --list)"
 
-fish_vi_key_bindings
+# use grc for specific commands
+set -U grc_wrap_commands cat df diff dig ifconfig netstat ping tail traceroute
+
+# https://github.com/nvbn/thefuck/wiki/shell-aliases
+eval (thefuck --alias | tr '\n' ';')
 
 source ~/.secrets.fish
