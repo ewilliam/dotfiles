4972d2b40aeb9cc5cf772bd1ebd8f901fc38fb50
William Albright <ping@ewilli.am>
2016-10-21 16:47:45 -0700

    fix pip alias, add fish env vars and plugins

--- cli/fish/config.fish
+++ cli/fish/config.fish
@@ -2,13 +2,19 @@ set -x EDITOR nvim
 
 fish_vi_key_bindings
 
-# Ruby version management
+# nerdfonts for bobthefish fish prompt
+set -g theme_nerd_fonts yes
+
+# point to hotel proxy in terminal
+# convert to function. global unneccessary
+# set -x http_proxy http://localhost:2000/proxy.pac
+
+# manage ruby versions
 # https://github.com/JeanMertz/chruby-fish
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
-chruby 2.3.1
 
-# Python version/envirnoment management
+# manage python versions/environments
 # https://github.com/adambrenecki/virtualfish
 # set -x PIP_REQUIRE_VIRTUALENV true
 set -x WORKON_HOME ~/.virtualenvs
@@ -33,4 +39,12 @@ eval (thefuck --alias | tr '\n' ';')
 set -x  OPENNI2_INCLUDE /usr/local/include/ni2
 set -x  OPENNI2_REDIST /usr/local/lib/ni
 
+# for Shopify Theme Kit
+set -U fish_user_paths /Users/ewilliam/.themekit $fish_user_paths
+
+# for rubymotion android
+set -x RUBYMOTION_ANDROID_SDK ~/.rubymotion-android/sdk
+set -x RUBYMOTION_ANDROID_NDK ~/.rubymotion-android/ndk
+
+source ~/.asdf/asdf.fish
 source ~/.secrets.fish
