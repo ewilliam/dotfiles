07dbe21c8910d1127b5bc49c4881a37802b7c537
William Albright <ping@ewilli.am>
2018-11-15 19:35:13 -0800

    improve greeting

--- fish/conf.d/greeting.fish
+++ fish/conf.d/greeting.fish
@@ -1,9 +1,8 @@
 function fish_greeting
   set_color --bold red
-  echo "Master William, get shit done."
+  echo "Master William, let's get shit done."
   set_color normal
 
   uname -npsr
   uptime
-  set_color normal
 end
