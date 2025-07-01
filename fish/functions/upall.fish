055c017205ae15ddcfd0b4936f32dcf62368c64e
William Albright <ping@ewilli.am>
2025-07-01 12:27:05 -0700

    set color in update everything function

--- fish/functions/upall.fish
+++ fish/functions/upall.fish
@@ -1,21 +1,21 @@
 function upall
-    echo "🚀 Starting system updates..."
+    set_color ffb6c1; echo "🚀 Starting system updates..."; set_color normal
     echo ""
 
     brewu
     pnpmu
     miseu
 
-    echo "🛍️ Running Mac App Store upgrades..."
+    set_color ffb6c1; echo "🛍️ Updating Mac App Store apps..."; set_color normal
     mas upgrade
-    echo "✅ Mac App Store upgrades completed."
+    set_color ffb6c1; echo "✅ Mac App Store updates completed."; set_color normal
     echo ""
 
-    echo "🐟 Updating Fisher packages..."
+    set_color ffb6c1; echo "🐟 Updating Fisher packages..."; set_color normal
     fisher update
-    echo "✅ Fisher updates completed."
+    set_color ffb6c1; echo "✅ Fisher updates completed."; set_color normal
     echo ""
 
-    echo "🎉🎉🎉 All system updates completed. 🎉🎉🎉"
+    set_color ffb6c1; echo "🎉🎉🎉 All updates completed. 🎉🎉🎉"; set_color normal
     echo ""
 end
