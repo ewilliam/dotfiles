99fbcd2868e60fa12e454cebe4a8a5351301a472
William Albright <ping@ewilli.am>
2025-07-17 13:36:43 -0700

    fix browser cache purge

--- fish/functions/clearbrowsercache.fish
+++ fish/functions/clearbrowsercache.fish
@@ -4,37 +4,37 @@ function clearbrowsercache -d "Clear browser caches on macOS"
     # Chrome
     if test -d ~/Library/Caches/Google/Chrome
         echo "Clearing Chrome cache..."
-        rm -rf ~/Library/Caches/Google/Chrome/*
+        rm -rf ~/Library/Caches/Google/Chrome/
     end
     
     # Firefox
     if test -d ~/Library/Caches/Firefox
         echo "Clearing Firefox cache..."
-        rm -rf ~/Library/Caches/Firefox/*
+        rm -rf ~/Library/Caches/Firefox/
     end
     
     # Safari
     if test -d ~/Library/Caches/com.apple.Safari
         echo "Clearing Safari cache..."
-        rm -rf ~/Library/Caches/com.apple.Safari/*
+        rm -rf ~/Library/Caches/com.apple.Safari/
     end
     
     # Edge
     if test -d ~/Library/Caches/com.microsoft.edgemac
         echo "Clearing Edge cache..."
-        rm -rf ~/Library/Caches/com.microsoft.edgemac/*
+        rm -rf ~/Library/Caches/com.microsoft.edgemac/
     end
     
     # Brave
     if test -d ~/Library/Caches/BraveSoftware/Brave-Browser
         echo "Clearing Brave cache..."
-        rm -rf ~/Library/Caches/BraveSoftware/Brave-Browser/*
+        rm -rf ~/Library/Caches/BraveSoftware/Brave-Browser/
     end
     
     # Arc
     if test -d ~/Library/Caches/company.thebrowser.Browser
         echo "Clearing Arc cache..."
-        rm -rf ~/Library/Caches/company.thebrowser.Browser/*
+        rm -rf ~/Library/Caches/company.thebrowser.Browser/
     end
     
     echo "Browser cache clearing complete!"
